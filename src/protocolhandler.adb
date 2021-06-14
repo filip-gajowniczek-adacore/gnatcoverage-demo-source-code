82b31450f2844dbe5d66b79e64385b2bed9136ce
Filip Gajowniczek <gajowniczek@adacore.com>
2021-06-14 14:21:26 -0500

    Changed ProtocolHandler to use multiple flags to more easily show differences between coverage types

--- src/protocolhandler.adb
+++ src/protocolhandler.adb
@@ -11,25 +11,26 @@ package body ProtocolHandler is
    Alarm : Running_Average_Alarm_Type(5);
    
    procedure Run is
-      Input : String(1..1);
+      C1 : String(1..1);
+      C2 : String(1..1);
+      Read1_Flag : Boolean;
+      Read2_Flag : Boolean;
       Alarm_Activated : Boolean;
    begin 
-      -- Check if the first character is newline
-      if Get_Input( Seconds(10), Input(1) ) and then 
-        Input(1) in Digit_Type then
-         if Input(1) = '1' then
-            -- Read the next character
-            if Get_Input( Seconds(0), Input(1)) and then
-              Input(1) = '0' then
-               -- This is a 10
+      
+      Read1_Flag := Get_Input (Seconds(5), C1(1));
+      Read2_Flag := Get_Input (Seconds(5), C2(1));
+      
+      if Read1_Flag and then Read2_Flag then
+         if C1(1) = '1' then 
+            if C2(1) = '0' then 
                Alarm_Activated := Alarm.Update(10);
-            elsif Input(1) in '1' .. '9' then
-               -- This is a 1 plus non-zero digit
-               Alarm_Activated := Alarm.Update (1);
-               Alarm_Activated := Alarm.Update (Integer'Value(Input));
+            elsif C2(1) in '1' .. '9' then
+               Alarm_Activated := Alarm.Update(1);
+               Alarm_Activated := Alarm.Update (Integer'Value(C2));
             end if;
-         else 
-            Alarm_Activated := Alarm.Update (Integer'Value(Input));
+         elsif C1(1) in '0' .. '9' then
+            Alarm_Activated := Alarm.Update (Integer'Value(C1));
          end if;
       end if;
       
